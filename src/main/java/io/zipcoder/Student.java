0692c21855d21533f799d98b9cc87a149892ce5b
jorge <jorgetapia0293@gmail.com>
2021-03-09 16:52:48 -0500

    got first test to pass

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,31 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    Double[] examScores;
+
+    public Student(String firstName, String lastName, Double[] examScores) {
+    }
+        public void setFirstName (){
+            this.firstName = firstName;
+        }
+        public void setLastName (){
+            this.lastName = lastName;
+        }
+        public String getFirstName (String firstName){
+            return firstName;
+        }
+        public String getLastName (String lastName){
+            return lastName;
+        }
+        public String getExamScores (Double[]examScores){
+            return examScores.toString();
+        }
+        public Integer getNumberOfExamsTaken ( double[] examScores){
+            return examScores.length;
+        }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
+    @Test
+    public void studentGetExamScoresTest(){
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = { 100.0, 95.0, 123.0, 96.0 };
+        Student student = new Student(firstName, lastName, examScores);
+
+// When
+        String output = student.getExamScores(examScores);
+
+// Then
+        System.out.println(output);
+    }
 
 }
