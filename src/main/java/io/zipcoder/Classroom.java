53044d3dcd36cdb3900af777d1f6c98ceea83442
jorge <jorgetapia0293@gmail.com>
2021-03-19 19:34:59 -0400

    tests passed

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,53 @@
 package io.zipcoder;
 
 public class Classroom {
+    Student[] students;
+
+    public Classroom(){
+        students = new Student[30];
+    }
+
+    public Classroom(Integer maxSize){
+        students =  new Student[maxSize];
+    }
+
+    public Classroom(Student[] students){
+        this.students = students;
+    }
+    public Double getAverageExamScore() {
+        Double sum = 0.0;
+        for (Student each: students) {
+            for (int i = 0; i < each.getNumberOfExamsTaken() ; i++) {
+                sum += each.examScores.get(i);
+            }
+        }
+
+
+        return (sum / students.length);
+    }
+
+    public void addStudent(Student student){
+
+        if (students[0] == null)
+            students[0] = student;
+        else {
+            Student[] temp = new Student[this.students.length + 1];
+            for (int i = 0; i < students.length; i++) {
+                temp[i] = students[i];
+            }
+            temp[temp.length - 1] = student;
+            students = temp;
+        }
+
+    }
+
+    public void removeStudent(String firstName, String lastName){
+
+    }
+
+
+
+    public Student[] getStudents() {
+        return students;
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,54 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
 public class ClassroomTest {
+    @Test
+    public void testAvg(){
+        // : Given
+        Double[] s1Scores = { 100.0, 150.0 };
+        Double[] s2Scores = { 225.0, 25.0 };
+
+        Student s1 = new Student("student", "one", s1Scores);
+        Student s2 = new Student("student", "two", s2Scores);
+
+        Student[] students = new Student[]{s1,s2};
+        Classroom classroom = new Classroom(students);
+
+// When
+        double output = classroom.getAverageExamScore();
+
+// Then
+        System.out.println(output);
+
+        Assert.assertEquals(250.0, classroom.getAverageExamScore(), .5);
+    }
+
+    @Test
+    public void testAdd(){
+        // : Given
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+
+// When
+        classroom.addStudent(student);
+        Student [] preEnrollment = classroom.getStudents();
+        classroom.addStudent(new Student("Jorge", "Tapia", examScores));
+        Student [] postEnrollment = classroom.getStudents();
+
+
+// Then
+        String preEnrollmentAsString = Arrays.toString(preEnrollment);
+        String postEnrollmentAsString = Arrays.toString(postEnrollment);
+
+        System.out.println("===========================");
+        System.out.println(preEnrollmentAsString);
+        System.out.println("===========================");
+        System.out.println(postEnrollmentAsString);
+    }
 }
